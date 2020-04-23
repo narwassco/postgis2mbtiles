248d1b3962bc10b85d2d7e15afa7e20eb506e69a
Jin IGARASHI <wushilan0623@gmail.com>
2020-04-23 22:21:43 +0900

    changed sql for meters

--- example/config.js
+++ example/config.js
@@ -56,7 +56,7 @@ module.exports = {
               ST_AsGeoJSON(ST_TRANSFORM(x.geom,4326))::json AS geometry,
               row_to_json((
                 SELECT p FROM (
-                SELECT
+                  SELECT
                   x.meterid as fid,
                   a.name as metertype,
                   x.pipesize,
@@ -64,6 +64,7 @@ module.exports = {
                   x.connno,
                   x.installationdate,
                   x.serialno,
+				          b.name as customer,
                   x.insertdate,
                   x.updatedate,
                   x.isgrantprj
@@ -72,6 +73,9 @@ module.exports = {
               FROM meter x
               INNER JOIN metertype a
               ON x.metertypeid = a.metertypeid
+              LEFT JOIN customer b
+              ON x.zonecd = b.zonecd
+              AND x.connno = b.connno
               WHERE NOT ST_IsEmpty(x.geom)
             ) AS feature
           ) AS featurecollection
